c93a1e1cc2c8c0f58f612f7299b2b9caf91d33f0
Jurredr <github@jurre.me>
2021-06-30 17:55:46 +0200

    Collapsing system added

--- components/WidgetWindow.vue
+++ components/WidgetWindow.vue
@@ -5,11 +5,85 @@
   >
     <div
       v-if="!collapsed"
-      class="title w-full h-14 bg-dark rounded-t-3xl"
-    ></div>
+      class="
+        title
+        w-full
+        h-14
+        bg-dark
+        rounded-t-3xl
+        flex
+        justify-center
+        items-center
+      "
+    >
+      <div
+        class="
+          minimize-button
+          absolute
+          left-4
+          cursor-pointer
+          rounded-3xl
+          bg-light
+          w-8
+          h-6
+          flex
+          justify-center
+          items-center
+        "
+        @click="collapsed = true"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16.448"
+          height="3.584"
+          viewBox="0 0 16.448 3.584"
+        >
+          <path
+            id="Icon_awesome-window-minimize"
+            data-name="Icon awesome-window-minimize"
+            d="M14.906,24.75H1.542A1.454,1.454,0,0,0,0,26.094v.9a1.454,1.454,0,0,0,1.542,1.344H14.906a1.454,1.454,0,0,0,1.542-1.344v-.9A1.454,1.454,0,0,0,14.906,24.75Z"
+            transform="translate(0 -24.75)"
+            fill="#fff"
+          />
+        </svg>
+      </div>
+      <p class="text-white font-semibold text-lg">{{ pages[currentPage] }}</p>
+    </div>
 
-    <div class="content flex justify-center">
-      <div v-if="collapsed" class="uncollapse"></div>
+    <div class="content-container flex justify-center">
+      <div class="content"></div>
+      <div
+        v-if="collapsed"
+        class="
+          uncollapse
+          absolute
+          cursor-pointer
+          rounded-3xl
+          bg-dark
+          w-10
+          h-6
+          flex
+          justify-center
+          items-center
+          top-2
+        "
+        @click="collapsed = false"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16.395"
+          height="9.373"
+          viewBox="0 0 16.395 9.373"
+        >
+          <path
+            id="Icon_ionic-ios-arrow-up"
+            data-name="Icon ionic-ios-arrow-up"
+            d="M14.387,14.076l6.2,6.2A1.172,1.172,0,1,0,22.24,18.62l-7.023-7.028a1.169,1.169,0,0,0-1.615-.034L6.529,18.615a1.172,1.172,0,0,0,1.655,1.659Z"
+            transform="translate(-6.188 -11.251)"
+            fill="#fff"
+          />
+        </svg>
+      </div>
       <div
         class="navigation absolute bg-dark rounded-3xl bottom-6 w-10/12 h-12"
       ></div>
@@ -22,6 +96,8 @@ export default {
   data() {
     return {
       collapsed: false,
+      pages: ['News', 'Profile', 'Chat', 'Settings'],
+      currentPage: 0,
     }
   },
 }
