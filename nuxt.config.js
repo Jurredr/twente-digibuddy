6b0a1ec804bc66c6944368ef3b9147188a3b2c3e
Jurredr <github@jurre.me>
2021-06-30 14:33:40 +0200

    Added meta configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,122 @@
+const description = 'Putting digital Twente on the map!'
+const title = 'Twente Digibuddy'
+const image = ''
+const imageWidth = 0
+const imageHeigth = 0
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'twente-digibuddy',
+    title,
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: description,
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content:
+          'Twente, Digital, Digibuddy',
+      },
+      {
+        hid: 'subject',
+        name: 'subject',
+        content: 'Data Technology',
+      },
+      {
+        hid: 'language',
+        name: 'language',
+        content: 'NL',
+      },
+      {
+        hid: 'author',
+        name: 'author',
+        content: 'Chris & Jurre',
+      },
+
+      // OpenGraph tags
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: title,
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: '',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: image,
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: image,
+      },
+      {
+        hid: 'og:image:width',
+        property: 'og:image:width',
+        content: imageWidth,
+      },
+      {
+        hid: 'og:image:height',
+        property: 'og:image:height',
+        content: imageHeigth,
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: title,
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: description,
+      },
+
+      // Twitter Card tags
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary',
+      },
+      {
+        hid: 'twitter:site',
+        name: 'twitter:site',
+        content: '@JurredeRuiter',
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: title,
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: description,
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: image,
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
