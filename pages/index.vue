57445b8e64f54e42670550b1760887f7aa627e4f
Chris <chirssel@gmail.com>
2021-06-30 14:19:58 +0200

    Add a marker to the map

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,12 @@
       :center="{ lat: 52.21833, lng: 6.89583 }"
       :zoom="14"
       :options="{ mapId: '42c056fc8fd3543', disableDefaultUI: true }"
-    ></google-map>
+    >
+      <google-map-marker
+        :position="{ lat: 52.21833, lng: 6.89583 }"
+        icon="http://maps.google.com/mapfiles/kml/paddle/purple-blank.png"
+      />
+    </google-map>
   </div>
 </template>
 
