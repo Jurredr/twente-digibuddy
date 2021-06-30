48c5801c03d6e1f53004edc786836280d8acf019
Chris <chirssel@gmail.com>
2021-06-30 14:34:47 +0200

    Limit map to Enschede

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,20 @@
   <div class="w-full h-screen">
     <google-map
       :center="{ lat: 52.21833, lng: 6.89583 }"
-      :zoom="14"
-      :options="{ mapId: '42c056fc8fd3543', disableDefaultUI: true }"
+      :zoom="1"
+      :options="{
+        mapId: '42c056fc8fd3543',
+        disableDefaultUI: true,
+        restriction: {
+          latLngBounds: {
+            north: 52.25975249383111,
+            south: 52.17603712615544,
+            east: 6.992454528808594,
+            west: 6.794700622558593,
+          },
+          strictBounds: false,
+        },
+      }"
     >
       <google-map-marker
         :position="{ lat: 52.21833, lng: 6.89583 }"
