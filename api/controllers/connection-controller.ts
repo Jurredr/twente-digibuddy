3e2d89e31540a42d51008389358cff25311aeb23
Jurredr <github@jurre.me>
2021-07-01 17:31:56 +0200

    Full backend done

--- api/controllers/connection-controller.ts
+++ api/controllers/connection-controller.ts
@@ -4,9 +4,33 @@ async function createConnection(
   company1: String,
   company2: String,
   type: String
-) {}
+) {
+  return await connections
+    .create({
+      company1,
+      company2,
+      type,
+    })
+    .then((data) => {
+      return data
+    })
+    .catch((error: Error) => {
+      throw error
+    })
+}
 
-async function getConnections(company: String) {}
+async function getConnections(company: String) {
+  await connections
+    .find({ company1: company })
+    .then(async (personsList) => {
+      await connections.find({ company2: company }).then((personsList2) => {
+        return personsList.concat(personsList2)
+      })
+    })
+    .catch((error: Error) => {
+      throw error
+    })
+}
 
 export default {
   createConnection,
