7c08f0cfdc8be39cfad73ff1d908fd5a9c331d78
Jurredr <github@jurre.me>
2021-07-01 18:46:09 +0200

    Info Window for companies fully done

---
+++ components/InfoFeature.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex items-center gap-4">
+    <p class="text-2xl">{{ icon }}</p>
+    <div class="flex-col">
+      <p class="font-semibold text-sm text-pink">{{ value }}</p>
+      <p class="font-regular text-sm text-gray">{{ name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: '💼',
+    },
+    name: {
+      type: String,
+      default: 'name',
+    },
+    value: {
+      type: String,
+      default: 'value',
+    },
+  },
+}
+</script>
+
+<style></style>
